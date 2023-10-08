cab1d1b3a63c27e457fe8311879e57c34e44f32d
Radosław Stawiszyński <radoslaw.stawiszynski@gmail.com>
2023-10-08 22:06:56 +0200

    Zapisuje aktualny postęp pracy

--- 1 - ultimate-react-course-RS/03-2-challange-dev-profile/dev-profile/src/index.js
+++ 1 - ultimate-react-course-RS/03-2-challange-dev-profile/dev-profile/src/index.js
@@ -1,15 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
 
 function App() {
   return (
     <div className="card">
-      <Avatar />
+      <Avatar name="Radosław Stawiszyński" photoName="rs-foto.JPEG" />
       <div className="data">
         <Intro />
         {/* Should contain one Skill component
@@ -21,13 +17,35 @@ function App() {
   );
 }
 
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
+function Avatar(props) {
+  return (
+    <div className="avatar">
+      <img src={props.photoName} alt={props.name} />
+    </div>
+  );
+}
+// opis osoby
+function Intro() {
+  <div className="skill-list"></div>;
+}
 
+// function SkillList() {
+//   <div className="skill">
+//     <Skill skill="HTML" level={90} />
+//     <Skill skill="CSS" level={80} />
+//     <Skill skill="JavaScript" level={70} />
+//     <Skill skill="React" level={60} />
+//     <Skill skill="Redux" level={50} />
+//     <Skill skill="Node.js" level={5} />
+//     <Skill skill="Express.js" level={0} />
+//     <Skill skill="MongoDB" level={0} />
+//     <Skill skill="PostgreSQL" level={0} />
+//   </div>;
+// }
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <StrictMode>
+  <React.StrictMode>
     <App />
-  </StrictMode>
+  </React.StrictMode>
 );
-
-reportWebVitals();
