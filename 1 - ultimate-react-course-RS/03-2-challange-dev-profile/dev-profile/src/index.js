1c51c562d959254f09fc74158c7c7c8ec3c77768
Radosław Stawiszyński <radoslaw.stawiszynski@gmail.com>
2023-10-09 22:54:58 +0200

    Completed portfolio project with the following updates: - Created and styled a personal card component - Added an avatar image and introductory text - Designed a skill list with dynamic backgrounds - Integrated React for a smooth user interface - Wrapped the entire app in a StrictMode
    Ready to showcase my web development skills!

--- 1 - ultimate-react-course-RS/03-2-challange-dev-profile/dev-profile/src/index.js
+++ 1 - ultimate-react-course-RS/03-2-challange-dev-profile/dev-profile/src/index.js
@@ -5,7 +5,7 @@ import "./styles.css";
 function App() {
   return (
     <div className="card">
-      <Avatar name="Radosław Stawiszyński" photoName="rs-foto.JPEG" />
+      <Avatar />
       <div className="data">
         <Intro />
         {/* Should contain one Skill component
@@ -17,31 +17,73 @@ function App() {
   );
 }
 
-function Avatar(props) {
+function Avatar() {
   return (
     <div className="avatar">
-      <img src={props.photoName} alt={props.name} />
+      <img
+        src="rs-foto.JPEG"
+        alt="Radosław Stawiszyński"
+        style={{ width: "100%", height: "auto" }}
+      />
     </div>
   );
 }
 // opis osoby
 function Intro() {
-  <div className="skill-list"></div>;
+  return (
+    <div>
+      <h1>"Radosław Stawiszyński"</h1>
+      <h3>
+        I'm a web developer with a passion for creating digital experiences. In
+        my free time, you'll often find me immersed in adventurous video games.
+      </h3>
+    </div>
+  );
 }
 
-// function SkillList() {
-//   <div className="skill">
-//     <Skill skill="HTML" level={90} />
-//     <Skill skill="CSS" level={80} />
-//     <Skill skill="JavaScript" level={70} />
-//     <Skill skill="React" level={60} />
-//     <Skill skill="Redux" level={50} />
-//     <Skill skill="Node.js" level={5} />
-//     <Skill skill="Express.js" level={0} />
-//     <Skill skill="MongoDB" level={0} />
-//     <Skill skill="PostgreSQL" level={0} />
-//   </div>;
-// }
+function SkillList() {
+  return (
+    <div className="skill-list">
+      <Skill skill="HTML" level={40} ico={"\u{1F44D}"} background="#FFEEE4" />
+      <Skill skill="CSS" level={40} ico={"\u{1F44D}"} background="gold" />
+      <Skill
+        skill="JavaScript"
+        level={40}
+        ico={"\u{1F44D}"}
+        background="#fc913a"
+      />
+      <Skill skill="React" level={80} ico={"\u{1F4AA}"} background="silver" />
+      <Skill skill="Python" level={50} ico={"\u{1F44D}"} background="#D7FFF1" />
+      <Skill skill="Node.js" level={5} ico={"\u{1F476}"} background="#D09E88" />
+      <Skill
+        skill="Git and GitHub"
+        level={80}
+        ico={"\u{1F4AA}"}
+        background="#8EC0E4"
+      />
+      <Skill
+        skill="Web Design"
+        level={50}
+        ico={"\u{1F44D}"}
+        background="#F6B352"
+      />
+    </div>
+  );
+}
+
+function Skill(props) {
+  const style = {
+    backgroundColor: props.background,
+  };
+
+  return (
+    <div className="skill" style={style}>
+      <h3>{props.skill}</h3>
+      <p>{props.level}%</p>
+      <span>{props.ico}</span>
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
