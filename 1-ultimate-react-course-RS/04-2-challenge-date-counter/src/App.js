255c552f2a7e034b3801d9c84c17cec454494265
Radosław Stawiszyński <radoslaw.stawiszynski@gmail.com>
2023-10-26 23:17:27 +0200

    feat: Implement a counter app with date display
    - Created a counter app with increment and decrement buttons for "Step" and "Count."
    - Used React and useState for managing state.
    - Displayed the current date in a user-friendly format.
    - Added conditional message logic based on the "Count" value.
    - To be continued: Refactor date display and improve styles.

--- 1-ultimate-react-course-RS/04-2-challenge-date-counter/src/App.js
+++ 1-ultimate-react-course-RS/04-2-challenge-date-counter/src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import React from "react";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Counter />
     </div>
   );
 }
 
-export default App;
+function Counter() {
+  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(0);
+
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = date.getMonth(); // miesiące numerowane są od 0 (styczeń) do 11 (grudzień)
+  const dayMonth = date.getDate();
+  const day = date.getDay();
+  const newDate = ` ${day} ${month + 1} ${dayMonth} ${year}`;
+
+  function leftButtonStep() {
+    setStep((step) => step - 1);
+  }
+  function leftButtonCount() {
+    setCount((count) => count - step);
+  }
+
+  function rightButtonStep() {
+    setStep((step) => step + 1);
+  }
+  function rightButtonCount() {
+    setCount((count) => count + step);
+  }
+
+  function Message({ count, newDate }) {
+    if (count === 0) {
+      return <p>{"Today is " + newDate}</p>;
+    } else if (count > 0) {
+      return <p>{count + "days from today is" + newDate}</p>;
+    } else {
+      return <p>{"Today is 2 " + newDate}</p>;
+    }
+  }
+
+  return (
+    <div>
+      <div className="firstRow">
+        <button className="leftButtonStep" onClick={leftButtonStep}>
+          -
+        </button>
+        <p>Step: {step} </p>
+        <button className="rightButtonStep" onClick={rightButtonStep}>
+          +
+        </button>
+      </div>
+
+      <div className="secondRow">
+        <button className="leftButtonCount" onClick={leftButtonCount}>
+          -
+        </button>
+        <p>Count: {count} </p>
+        <button className="rightButtonCount" onClick={rightButtonCount}>
+          +
+        </button>
+        <Message count={count} newDate={newDate} />
+      </div>
+    </div>
+  );
+}
