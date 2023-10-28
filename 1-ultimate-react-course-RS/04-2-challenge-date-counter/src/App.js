588f2425c1200a05a1a82da993ef2c70d01344f4
Radosław Stawiszyński <radoslaw.stawiszynski@gmail.com>
2023-10-28 14:38:09 +0200

    Implemented a basic counter application with step and count functionality. This commit covers the App.js and related components.

--- 1-ultimate-react-course-RS/04-2-challenge-date-counter/src/App.js
+++ 1-ultimate-react-course-RS/04-2-challenge-date-counter/src/App.js
@@ -1,9 +1,10 @@
 import { useState } from "react";
 import React from "react";
+import "./styles.css";
 
 export default function App() {
   return (
-    <div>
+    <div className="App">
       <Counter />
     </div>
   );
@@ -13,59 +14,53 @@ function Counter() {
   const [step, setStep] = useState(1);
   const [count, setCount] = useState(0);
 
-  const date = new Date();
-  const year = date.getFullYear();
-  const month = date.getMonth(); // miesiące numerowane są od 0 (styczeń) do 11 (grudzień)
-  const dayMonth = date.getDate();
-  const day = date.getDay();
-  const newDate = ` ${day} ${month + 1} ${dayMonth} ${year}`;
-
-  function leftButtonStep() {
-    setStep((step) => step - 1);
-  }
-  function leftButtonCount() {
-    setCount((count) => count - step);
-  }
-
-  function rightButtonStep() {
-    setStep((step) => step + 1);
-  }
-  function rightButtonCount() {
-    setCount((count) => count + step);
-  }
-
-  function Message({ count, newDate }) {
-    if (count === 0) {
-      return <p>{"Today is " + newDate}</p>;
-    } else if (count > 0) {
-      return <p>{count + "days from today is" + newDate}</p>;
-    } else {
-      return <p>{"Today is 2 " + newDate}</p>;
-    }
-  }
+  const calculatedCount = count * 24 * 3600; // Przykład: count= ilość dni * ilość godz na dobe * ilosć sekund w godzinie = wynik podany w sekundach
+  const currentDate = new Date();
+  const newDate = new Date(currentDate.getTime() + calculatedCount * 1000); // Dodanie lub odjęcie czasu w sekundach
 
   return (
     <div>
-      <div className="firstRow">
-        <button className="leftButtonStep" onClick={leftButtonStep}>
+      <div className="RowButton">
+        <button
+          className="ButtonStep"
+          onClick={() => setStep((step) => step - 1)}
+        >
           -
         </button>
-        <p>Step: {step} </p>
-        <button className="rightButtonStep" onClick={rightButtonStep}>
+        <span> Step: {step} </span>
+        <button
+          className="ButtonStep"
+          onClick={() => setStep((step) => step + 1)}
+        >
           +
         </button>
       </div>
 
-      <div className="secondRow">
-        <button className="leftButtonCount" onClick={leftButtonCount}>
+      <div className="RowButton">
+        <button
+          className="ButtonCount"
+          onClick={() => setCount((count) => count - step)}
+        >
           -
         </button>
-        <p>Count: {count} </p>
-        <button className="rightButtonCount" onClick={rightButtonCount}>
+        <span> Count: {count} </span>
+        <button
+          className="ButtonCount"
+          onClick={() => setCount((count) => count + step)}
+        >
           +
         </button>
-        <Message count={count} newDate={newDate} />
       </div>
+      <p>
+        <span>
+          {count === 0
+            ? "Today is "
+            : count > 0
+            ? `${count} days from today is `
+            : `${Math.abs(count)} days ago was `}
+        </span>
+        <span>{newDate.toDateString()}</span>
+      </p>
     </div>
   );
 }
