5e8e639fae369d8048b98c13c342cc2aa903b171
Radosław Stawiszyński <radoslaw.stawiszynski@gmail.com>
2024-04-11 14:49:32 +0200

    2 version of Steps

--- 04-1-REACT-steps/src/App-v1.js
+++ 04-1-REACT-steps/src/App-v1.js
@@ -10,15 +10,7 @@ export default function App() {
   return (
     <div>
       <Steps />
-      {/* <Steps /> */}
-      <StepMessage step={"First"}>
-        <p>Pass in content</p>
-        <p>🙄</p>
-      </StepMessage>
-      <StepMessage step={"Second"}>
-        <p>Read children prop</p>
-        <p>🥱</p>
-      </StepMessage>
+      <Steps />
     </div>
   );
 }
@@ -67,65 +59,28 @@ function Steps() {
             <div className={step >= 2 ? "active" : ""}>2</div>
             <div className={step >= 3 ? "active" : ""}>3</div>
           </div>
-          <StepMessage step={step}>
-            <div>{messages[step - 1]}</div>
+          <p className="message">
+            Step {step}: {messages[step - 1]}
             {/*{test.name}*/}
-            <p></p>
-            <div className="buttons">
-              <Button2
-                bgColor="#e7e7e7"
-                textColor="#333"
-                onClick={() => alert(`Learn how to ${messages[step - 1]}`)}
-              >
-                Learn how
-              </Button2>
-            </div>
-          </StepMessage>
+          </p>
           <div className="buttons">
-            <Button1
-              bgColor="#7950f2"
-              textColor="#fff"
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
               onClick={handlePrevious}
-              text="Previous"
-              emoji="👈"
-            />
-            <Button2 bgColor="#7950f2" textColor="#fff" onClick={handleNext}>
-              <span>👉</span>
+              // onMouseEnter={() => alert("TEST")}
+              // po najechaniu myszką włącza się alert
+            >
+              Previous
+            </button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handleNext}
+            >
               Next
-            </Button2>
+            </button>
           </div>
         </div>
       )}
     </div>
-  );
-}
-function StepMessage({ step, children }) {
-  return (
-    <div className="message">
-      <h3>Step {step}</h3>
-      {children}
-    </div>
-  );
-}
-
-function Button1({ textColor, bgColor, onClick, text, emoji }) {
-  return (
-    <button
-      style={{ backgroundColor: bgColor, color: textColor }}
-      onClick={onClick}
-    >
-      <span>{emoji}</span>
-      {text}
-    </button>
-  );
-}
-function Button2({ textColor, bgColor, onClick, children }) {
-  return (
-    <button
-      style={{ backgroundColor: bgColor, color: textColor }}
-      onClick={onClick}
-    >
-      {children}
-    </button>
   );
 }
